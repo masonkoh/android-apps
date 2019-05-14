0f5069a9c2365f5124ac653059830977bc1781f6
ko.terra.119@gmail.com <ko.terra.119@gmail.com>
2019-05-14 15:57:42 -0400

    organizing directories (no code change)

--- intent/app/src/main/java/com/example/intent/MainActivity.java
+++ intent/app/src/main/java/com/example/intent/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.intent;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button page1Btn = (Button) findViewById(R.id.intentBtn1);
+        Button page2Btn = (Button) findViewById(R.id.intentBtn2);
+        Button page3Btn = (Button) findViewById(R.id.intentBtn3);
+
+
     }
 }
